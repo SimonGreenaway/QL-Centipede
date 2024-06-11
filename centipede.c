75697b5c78f837b27a6c2a8bf1094d5e52cde2ba
SimonGreenaway <simon@simongreenaway.com>
2024-06-11 20:04:51 +0100

    Player can shoot mushrooms.

--- centipede.c
+++ centipede.c
@@ -98,7 +98,7 @@ int main(int argc,char *argv[])
 	{
 		unsigned int f=getFrames();
 
-		if(f>player.timer.value)
+		if(f>player.timer.value) // Time to move the player?
 		{
 			unsigned int key=keyrow(1);
 			unsigned int newx=player.x,newy=player.y;
@@ -108,15 +108,19 @@ int main(int argc,char *argv[])
 			if((key&4)&&(player.y>(256-5*8))) newy-=2;
 			if((key&128)&&(player.y<(255-8-1))) newy+=2;
 
+			// Move the player?
 			if((player.x!=newx)||(player.y!=newy))
 			{
 				player.mask=1; player.draw=0;
 				spritePlot(SCREEN,&player);
+
 				player.x=newx; player.y=newy;
+
 				player.mask=0; player.draw=1;
 				spritePlot(SCREEN,&player);
 			}
 
+			// Fire a bullet?
 			if((key&64)&&!player_bullet.active)
 			{
 				player_bullet.active=1;
@@ -129,20 +133,45 @@ int main(int argc,char *argv[])
 			player.timer.value=getFrames()+1;
 		}
 
+		// Move player bullet?
 		if(player_bullet.active&&(f>player_bullet.timer.value))
 		{
 			player_bullet.mask=1; player_bullet.draw=0;
 			spritePlot(SCREEN,&player_bullet);
 
-			if(player_bullet.y<4)
+			if(player_bullet.y<8)
 			{
 				player_bullet.active=0;
 			}
 			else
 			{
-				player_bullet.y-=4;
-				player_bullet.mask=0; player_bullet.draw=1;
-				spritePlot(SCREEN,&player_bullet);
+				player_bullet.y-=8;
+
+				if(mushrooms[(player_bullet.x-XPOS)/8][player_bullet.y/8].active)
+				{
+					sprite *m=&mushrooms[(player_bullet.x-XPOS)/8][player_bullet.y/8];
+					m->mask=1; m->draw=0;
+					spritePlot(SCREEN,m);
+
+					if(m->currentImage==3)
+					{
+						m->currentImage=0;
+						m->active=0;
+					}
+					else
+					{
+						m->currentImage++;
+						m->mask=0; m->draw=1;
+						spritePlot(SCREEN,m);
+					}
+
+					player_bullet.active=0;
+				}
+				else
+				{
+					player_bullet.mask=0; player_bullet.draw=1;
+					spritePlot(SCREEN,&player_bullet);
+				}
 			}
 
 			player_bullet.timer.value=f+1;
